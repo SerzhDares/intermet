d3fe54388fe74490bfd5f481e9747bab7255dfb3
SerzhDares <serghdares@yandex.ru>
2025-12-28 22:09:56 +0400

    add slider-banner

--- js/main.js
+++ js/main.js
@@ -3,3 +3,14 @@ openFormBtn.addEventListener('click', () => {
     openFormBtn.classList.add('invisible');
     document.querySelector('.application_form').classList.remove('invisible');
 })
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
